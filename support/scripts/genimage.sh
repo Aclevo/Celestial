825c803e00474d8a9406d8964eccd2181ea8eaea
dulstack <79407205+dulstack@users.noreply.github.com>
2025-09-09 19:16:10 +0200

    Update genimage.sh

--- support/scripts/genimage.sh
+++ support/scripts/genimage.sh
@@ -39,7 +39,8 @@ done
 
 trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="bin/build/buildroot-fs/rootfs.iso9660.tmp"
-cp -r binary/packages/x86_64/* $ROOTPATH_TMP/
+##Uncomment when necessary
+#cp -r binary/packages/x86_64/* $ROOTPATH_TMP/
 rm -rf "${GENIMAGE_TMP}"
 
 genimage \
